740e42b1ba1f89c58d30f1ce618872c7e20595fb
LRison <2909279991@qq.com>
2023-08-16 10:36:30 +0800

    新增员工代码

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -73,6 +74,24 @@ public class EmployeeController {
     @ApiOperation("员工退出")
     @PostMapping("/logout")
     public Result<String> logout() {
+        return Result.success();
+    }
+
+
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     * @return
+     */
+    @ApiOperation("新增员工")
+    @PostMapping
+    public Result save(@RequestBody EmployeeDTO employeeDTO) {
+
+        System.out.println("当前线程id"+Thread.currentThread().getId());
+
+        log.info("新增员工{}", employeeDTO);
+        employeeService.save(employeeDTO);
         return Result.success();
     }
 
